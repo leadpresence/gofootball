55607c70e4ce9226ad9737c9c9ec0283171b3ba8
ChiFelix <felix.chibueze@wayapaychat.com>
2022-05-04 07:54:43 +0100

    first ...||||||

--- app/src/main/java/com/gofootball/football/data/remote/ApiClient.kt
+++ app/src/main/java/com/gofootball/football/data/remote/ApiClient.kt
@@ -1,13 +1,6 @@
 package com.gofootball.football.data.remote
 
 import com.gofootball.football.model.MatchApiResponse
-import com.gofootball.football.model.fixture.FixtureResponse
-import com.gofootball.football.model.leaguetable.LeagueTableResponse
-import com.gofootball.football.model.player.PlayerResponse
-import com.gofootball.football.model.statistic.StatisticsResponse
-import com.gofootball.football.model.team.TeamResponse
-import com.gofootball.football.model.topscorer.TopScorerResponse
-import com.gofootball.football.model.transfer.TransferResponse
 import com.gofootball.football.util.Constant
 import io.reactivex.Single
 import retrofit2.Retrofit
--- app/src/main/java/com/gofootball/football/ui/MainActivity.kt
+++ app/src/main/java/com/gofootball/football/ui/MainActivity.kt
@@ -21,8 +21,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
-
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         navController = this.findNavController(R.id.fragment)
